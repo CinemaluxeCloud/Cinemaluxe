d1c5abf964bc3b54963c9af2123532c6fc136ff2
CinemaluxeCloud <149947629+CinemaluxeCloud@users.noreply.github.com>
2023-11-05 20:31:34 +0530

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -52,7 +52,7 @@ module.exports = {
   // you provide. See the documentation for details.
   links: [
     {
-      name: 'GitHub',
+      name: 'Telegram',
       link: 'https://github.com/Chiheisen-Union/Chiheisen-Vercel-Index',
     },
   ],
